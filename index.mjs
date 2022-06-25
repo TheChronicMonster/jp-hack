4737b133c71eaacc7b09fe265b1920e0d20c89f3
TheChronicMonster <legendzing@gmail.com>
2022-06-25 00:26:44 -0500

    feat: v1

---
+++ index.mjs
@@ -0,0 +1,83 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib();
+
+const startingBalance = stdlib.parseCurrency(100);
+
+console.log(`Creating test Assset`);
+const accCreator = await stdlib.newTestAccount(startingBalance);
+
+const theAsset = await stdlib.launchToken(accCreator, "Alloy game", "GAME", { supply: 1 });
+const assetId = theAsset.id;
+const LICENSETYPE = ['FREE', 'LIMITED', 'BONDING CURVE', 'FRACTIONAL', 'CUSTOM'];
+const shares = 1;
+const retailPrice = stdlib.parseCurrency(10);
+const minSec = stdlib.parseCurrency(0);
+const royalty = 5;
+const lenInBlocks = 10;
+const params = { assetId, shares, retailPrice, minSec, royalty, lenInBlocks };
+
+let done = false;
+const bidders = [];
+const startLicense = async () => {
+    let transaction = minSec;
+    const runBidder = async (who) => {
+        const inc = stdlib.parseCurrency(Math.random() * 10);
+        transaction = transaction.add(inc);
+
+        const acc = await stdlib.newTestAccount(startingBalance);
+        acc.setDebugLabel(who);
+        await acc.tokenAccept(nftId);
+        bidders.push([who, acc]);
+        const ctc = acc.contract(bakcend, ctcCreator.getInfo());
+        const getBal = async () => stdlib.formatCurrency(await stdlib.balanceOf(acc));
+
+        console.log(`${who} bids ${stdlib.formatCurrency(transaction)}.`);
+        console.log(`${who} balance before is ${await getBal()}`);
+        try {
+            const [ lastBidder, lastBid ] = await ctc.apis.Gamer.transaction(transaction);
+            console.log(`${who} out bid ${lastBidder} who bid ${stdlib.formatCurrency(lastBid)}.`);
+        } catch (e) {
+            console.log(`${who} failed to bid, because the auction is over`);
+        }
+        console.log(`${who} balance after is ${await getBal()}`);
+    };
+
+    await runBidder('Alice');
+    await runBidder('Bob');
+    await runBidder('Claire');
+    while (! done) {
+        await stdlib.wait(1);
+    }
+};
+
+const ctcCreator = accCreator.contract(backend);
+await ctcCreator.p.Creator({
+    setLicense: () => {
+        console.log(`Creator sets parameters of License:`, params);
+        return params;
+    },
+    isReady: () => {
+        startLicense();
+    },
+    seePrice: (who, amt) => {
+        console.log(`Creator saw that ${stdlib.formatAddress(who)} bid ${stdlib.formatCurrency(amt)}.`);
+    },
+    showOutcome: (purchaser, amt) => {
+        console.log(`Creator saw that ${stdlib.formatAddress(purchaser)} won with ${stdlib.formatCurrency(amt)}`);
+    },
+});
+
+// View //
+const proof = await ctc.views.Obs.proof();
+console.log(`The asset is owned by ${formatAddress(who, amt)}.`);
+const price = await ctc.views.Obs.price();
+console.log(`The price is ${parseCurrency(who, amt)}.`);
+const whoOwns = await ctc.views.Obs.whoOwns();
+console.log(`The asset is owned by ${formatAddress(who, amt)}.`);
+
+for (const [who, acc] of bidders) {
+    const [ amt, amtAsset ] = await stdlib.balancesOf(acc, [null, nftId]);
+    console.log(`${who} has ${stdlib.formatCurrency(amt)} ${stdlib.standardUnit} and ${amtAsset} of the Asset`);
+}
+done = true;
---
+++ index.rsh
@@ -0,0 +1,69 @@
+'reach 0.1';
+
+export const main = Reach.App(() => {
+    const Creator = Participant('Creator', {
+        setLicense: Fun([], Object({
+            assetId: Token,
+            licenseType: UInt,
+            shares: UInt,
+            retailPrice: UInt,
+            minSec: UInt,
+            royalty: UInt,
+            lenInBlocks: UInt,
+        })),
+        isReady: Fun([], Null),
+        seePrice: Fun([Address, UInt], Null),
+        showOutcome: Fun([Address, UInt], Null),
+    });
+    const Gamer = API('Gamer', {
+        transaction: Fun([UInt], Tuple(Address, UInt)),
+    });
+    const Obs = View('Obs', {
+        proof: Fun([Address, UInt], Null),
+        price: Fun([Address, UInt], Null),
+        whoOwns: Fun([Address, UInt], Null),
+    });
+    init();
+
+    Creator.only(() => {
+        const { assetId, licenseType, shares, retailPrice, minSec, royalty, lenInBlocks } = declassify(interact.setLicense());
+    });
+    Creator.publish(assetId, licenseType, shares, retailPrice, minSec, royalty, lenInBlocks);
+    const amt = 1;
+    commit();
+    Creator.pay([[amt, assetId]]);
+    Creator.interact.isReady();
+    assert(balance(assetId) == amt, "balance of asset is wrong");
+    const end = lastConsensusTime() + lenInBlocks;
+    
+    const [
+        highestTransaction,
+        lastPrice,
+        isFirstTransaction,
+    ] = parallelReduce([Creator, minSec, true])
+        .invariant(balance(assetId) == amt)
+        .invariant(balance() == (isFirstTransaction ? 0: lastPrice))
+        .while(lastConsensusTime() <= end)
+        .api_(Gamer.transaction, (transaction) => {
+            check(transaction > lastPrice, "transaction is too low");
+            return [ transaction, (notify) => {
+                notify([highestTransaction, lastPrice]);
+                if ( ! isFirstTransaction ) {
+                    transfer(lastPrice).to(highestTransaction);
+                }
+                const who = this;
+                Creator.interact.seePrice(who, transaction);
+                return [who, transaction, false];
+            }];
+        })
+        .timeout(absoluteTime(end), () => {
+            Creator.publish();
+            return [highestTransaction, lastPrice, isFirstTransaction];
+        });
+
+        transfer( amt, assetId ).to(highestTransaction);
+        if ( ! isFirstTransaction ) { transfer(lastPrice).to(Creator); }
+        Creator.interact.showOutcome(highestTransaction, lastPrice);
+    commit();
+    exit();
+});
